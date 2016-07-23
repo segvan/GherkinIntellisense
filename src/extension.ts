1ccbb7d327415f7b79c90cd1c93f563c9485b6d5
s.trotsenko@gmail.com <s.trotsenko@gmail.com>
2016-07-23 19:28:15 +0200

    basic intellisense

--- src/extension.ts
+++ src/extension.ts
@@ -2,23 +2,31 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import { GherkinProvider } from './GherkinProvider';
+
+var disposableProvider;
+var isProviderEnabled = false;
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
 
-    // Use the console to output diagnostic information (console.log) and errors (console.error)
-    // This line of code will only be executed once when your extension is activated
-    console.log('Congratulations, your extension "gherkinintelisense" is now active!');
-
     // The command has been defined in the package.json file
     // Now provide the implementation of the command with  registerCommand
     // The commandId parameter must match the command field in package.json
-    let disposable = vscode.commands.registerCommand('extension.sayHello', () => {
-        // The code you place here will be executed every time your command is executed
+    let disposable = vscode.commands.registerCommand('extension.gherkinintellisense', () => {        
+        if (isProviderEnabled) {
+            isProviderEnabled = false;
+            vscode.window.setStatusBarMessage('Gherkin Intellisense disabled');
+            vscode.Disposable.from(disposableProvider).dispose();
+        } else {
+            isProviderEnabled = true;
+            vscode.window.setStatusBarMessage('Gherkin Intellisense enabled');
+            var gherkinProvider = new GherkinProvider();
+            const triggers = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'v', 'w', 'x', 'y', 'z'];
+            disposableProvider = vscode.languages.registerCompletionItemProvider('*', gherkinProvider, ...triggers);
+        }
 
-        // Display a message box to the user
-        vscode.window.showInformationMessage('Hello World!');
     });
 
     context.subscriptions.push(disposable);
